ea6ad5253540a9e48bdb2a5e83969e590021c8ea
Benjamin Lewis <benjaminlewis984@gmail.com>
2020-09-24 09:09:22 -0700

    Created cmake and main.cpp files for procedural programming assignment

---
+++ cmake-build-debug/CMakeFiles/Procedural-Programming.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Procedural-Programming.dir/main.cpp.o"
+  "Procedural-Programming"
+  "Procedural-Programming.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Procedural-Programming.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,15 @@
+/**************************************************************
+* Class: CSC-600-01 # Fall 2020
+* Name: Procedural Programming
+* Student ID: 915173797
+* Name: Benjamin Lewis
+* **************************************************************/
+
+#include <iostream>
+
+int main(){
+
+    std::cout << "Yee" << std::endl;
+
+    return 0;
+}
