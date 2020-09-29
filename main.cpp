6a97546e48168f5fa3c3b61cceb3e57d180f5e8f
Benjamin Lewis <benjaminlewis984@gmail.com>
2020-09-29 11:58:39 -0700

    Started creation of arrayReduce() function which takes array and size of that array in order to reduce the array by the 3 largest elements. Stable

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@
 
 int maxlen(const int [], int);
 void bigInt(const int);
+int arrayReduce(int [], int);
 
 const char ascNumbers[SINGLE_DIGITS][RC_SIZE][RC_SIZE] = {
         {
@@ -122,6 +123,7 @@ int main() {
     int sizeOfSeq = sizeof(sequence)/ sizeof(sequence[0]);
     int mode = maxlen(sequence, sizeOfSeq);
     std::cout << "The mode is : " << mode << std::endl;
+    std::cout << std::endl;
 
 //    BigInt 3 dimensional array that shows ascii symbol @ written for integers 0 - 9
     int numPrint = 1;
@@ -137,7 +139,25 @@ int main() {
         }
     }
 
+//  Array Processing. Creating a function reduce(array, size) that reduces
+//  the given array's 3 largest integers. i.e a=(9,1,1,6,7,1,2,6,6,6,7,1,9)
+//  reduce(a, size) returns a=(1,1,1,2,1) since 6,7,9 are the 3 largest.
+
+    int array[16] = {9,1,1,6,7,1,2,3,3,5,6,6,6,6,7,9};
+    int arraySize = sizeof(array)/sizeof(array[0]);
+    int reducedArraySize = arrayReduce(array, arraySize);
+
     return 0;
+}
+
+int arrayReduce(int array[], int size)
+{
+    int largest[3];
+    int temp;
+    for (int i = 0; i < size; i++)
+    {
+
+    }
 }
 
  void bigInt(const int number)
